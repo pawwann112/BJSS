8e2e8eefeab22fa6f72fbf2b4f95af83db5b7327
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2024-11-05 16:32:09 +0100

    Update addUserToGroup.ps1

--- addUserToGroup.ps1
+++ addUserToGroup.ps1
@@ -1,12 +1,37 @@
-$vcFqdn = 'ch-sv01993.group.intra'
-$vcUser = 'administrator@vs6bjss.local'
-$vcPassword = 'hzZcL$fT8wGz9'
-$ldapDomain = 'group.intra'
-$ldapUser = 'adm_arsi'
-$ssoDomain = 'vs6bjss.local'
-$ssoGroup = "BJSS-VM-Power-User-DLP-INTERNATIONAL"
+# Input Parameters
+param (
+    [string]$vcFqdn = 'ch-sv01993.group.intra',
+    [string]$vcUser = 'administrator@vs6bjss.local',
+    [string]$vcPassword = 'hzZcL$fT8wGz9',
+    [string]$ldapDomain = 'group.intra',
+    [string]$csvFilePath,    # Path to CSV file containing list of users
+    [string]$ssoGroup        # SSO group to add users to
+)
 
+# Connect to the SSO Admin Server
 $ssoConnection = Connect-SsoAdminServer -Server $vcFqdn -User $vcUser -Password $vcPassword -SkipCertificateCheck
-$targetGroup = Get-SsoGroup -Domain $ssoDomain -Name $ssoGroup -Server $ssoConnection
-$ldapUserToAdd = Get-SsoPersonUser -Domain $ldapDomain -Name $ldapUser -Server $ssoConnection
-$ldapUserToAdd | Add-UserToSsoGroup -TargetGroup $targetGroup
+
+# Get the target SSO group
+$targetGroup = Get-SsoGroup -Domain 'vs6bjss.local' -Name $ssoGroup -Server $ssoConnection
+
+# Import users from CSV file
+$users = Import-Csv -Path $csvFilePath
+
+# Loop through each user in the CSV and add to the SSO group
+foreach ($user in $users) {
+    $userName = $user.UserName   # Assume 'UserName' is the column name in the CSV
+
+    # Get the user from the SSO domain
+    $ldapUserToAdd = Get-SsoPersonUser -Domain $ldapDomain -Name $userName -Server $ssoConnection
+
+    # Add user to the SSO group
+    if ($ldapUserToAdd) {
+        $ldapUserToAdd | Add-UserToSsoGroup -TargetGroup $targetGroup
+        Write-Host "Added user $userName to group $ssoGroup."
+    } else {
+        Write-Host "User $userName not found in domain $ldapDomain."
+    }
+}
+
+# Disconnect from the SSO Admin Server
+Disconnect-SsoAdminServer -Server $ssoConnection
